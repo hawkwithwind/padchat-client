0df14ef32276a52ef6a749fce7493c84ad61bba3
潘小强 <hawkwithwind@gmail.com>
2018-11-07 19:31:05 +0800

    fix run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,9 @@
 docker run -it --rm --net=host \
        -v `pwd`/libs:/home/work/libs \
        -v `pwd`/proto:/home/work/proto \
+       -v `pwd`/node_modules:/home/work/node_modules \
+       -v `pwd`/package.json:/home/work/package.json \
+       -v `pwd`/package-lock.json:/home/work/package-lock.json \
        -v `pwd`/main.js:/home/work/main.js \
        -v `pwd`/$1/config:/home/work/config \
        -v `pwd`/$1/logs:/home/work/logs \
