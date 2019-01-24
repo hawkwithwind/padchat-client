490e5deae3e8a3265c7158eb582ceeefce7cc635
潘小强 <hawkwithwind@gmail.com>
2019-01-24 10:53:09 +0800

    added node-zabbix-sender

--- run_npm.sh
+++ run_npm.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+#!/bin/bash 
 
-docker run -it --rm --net=host -v `pwd`:/home/work -w /home/work npm:padchat-client "$@"
+docker run -it --rm --net=host \
+       -v `pwd`:/home/work \
+       --env HTTPS_PROXY=$https_proxy \
+       --env HTTP_PROXY=$http_proxy \
+       -w /home/work npm:padchat-client "$@"
 
